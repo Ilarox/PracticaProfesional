06e3514df25f643fc9b036c3c0d5a773b8b29df1
Ilarox <rojas.cintia11@gmail.com>
2022-05-09 22:17:51 -0300

    Cambio de pantalla de registro de capitana por registro de equio

--- formulario.js
+++ formulario.js
@@ -4,22 +4,23 @@ const formulario = document.getElementById('formulario');
 const expresionesRegulares = {
 	//usuario: /^[a-zA-Z0-9\_\-]{4,16}$/, // Letras, numeros, guion y guion_bajo
 	nombrePatron: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
-    apellidoPatron: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
-	dniPatron:/^\d{8}$/, // 8 numeros.
+    //apellidoPatron: /^[a-zA-ZÀ-ÿ\s]{3,40}$/,
+	//dniPatron:/^\d{8}$/, // 8 numeros.
     emailPatron: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.([a-zA-Z]{2,4})+$/,
-    telefonoPatron: /^\d{7,14}$/, // 7 a 14 numeros.
-    passwordPatron: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{8,15}/, 
-	passwordPatron2: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d$@$!%*?&]{8,15}/
+    //telefonoPatron: /^\d{7,14}$/, // 7 a 14 numeros.
+    passwordPatron: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/,
+	passwordPatron2: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/
 }
 
 
 var nombre= document.getElementById('nombre');
-var apellido= document.getElementById('apellido');
-var dni= document.getElementById('dni');
+//var //apellido= document.getElementById('apellido');
+//var dni= document.getElementById('dni');
 var email= document.getElementById('email');
-var telefonoCelular= document.getElementById('telefonoCelular');
+//var //telefonoCelular= document.getElementById('telefonoCelular');
 var password1= document.getElementById('password1');
 var password2= document.getElementById('password2');
+var cantidadJugadoras = document.getElementById('cantidadJugadoras'); //tomo el valor ingresado de cantidad jugadoras
 
 
 function enviarFormulario() {
@@ -31,20 +32,8 @@ function enviarFormulario() {
     else{
         document.getElementById('errorN').innerHTML = "";
     }
-    if(apellido.value== null || apellido.value== "" || expresionesRegulares.apellidoPatron.test(apellido.value)==false) 
-    {
-        document.getElementById('errorA').innerHTML = "Ingrese correctamente su apellido";
-    }
-    else{
-        document.getElementById('errorA').innerHTML = "";
-    }
-    if(dni.value== null || dni.value== "" ||  expresionesRegulares.dniPatron.test(dni.value)==false) 
-    {
-        document.getElementById('errorDNI').innerHTML = "Ingrese correctamente su DNI";
-    }
-    else{
-        document.getElementById('errorDNI').innerHTML = "";
-    }
+   
+   
     if(email.value== null || email.value== "" || expresionesRegulares.emailPatron.test(email.value)==false) 
     {
         document.getElementById('errorE').innerHTML = "Ingrese correctamente su email";
@@ -52,13 +41,7 @@ function enviarFormulario() {
     else{
         document.getElementById('errorE').innerHTML = "";
     }
-    if(telefonoCelular.value== null || telefonoCelular.value== "" || expresionesRegulares.telefonoPatron.test(telefonoCelular.value)==false) 
-    {
-        document.getElementById('errorTC').innerHTML = "Ingrese correctamente su teléfono";
-    }
-    else{
-        document.getElementById('errorTC').innerHTML = "";
-    }
+   
     if(password1.value== null || password1.value== "" || expresionesRegulares.passwordPatron.test(password1.value)==false)
     {
         document.getElementById('errorP1').innerHTML = "Ingrese correctamente su contraseña. Formato de 8 a 15 dígitos, mínimo 1 mayúscula, 1 minúscula, 1 número y 1 carácter (.-@$!%*?&)";
@@ -80,5 +63,13 @@ function enviarFormulario() {
     else{
         document.getElementById('errorP2').innerHTML = "";
     }
+
+    if(cantidadJugadoras.value== null || cantidadJugadoras.value== "" || cantidadJugadoras.value > 15 || cantidadJugadoras.value < 5)
+    {
+        document.getElementById('errorCantidadJugadoras').innerHTML = "Debe ingresar entre 5 y 15 jugadoras";
+    } else {
+        document.getElementById('errorCantidadJugadoras').innerHTML = "";
+    }
+    
     return false;
 }
