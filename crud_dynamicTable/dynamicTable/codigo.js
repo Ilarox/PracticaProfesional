a3b6314cc6b594e77de9c9fa09ac5f6d95e1fa91
Ilarox <rojas.cintia11@gmail.com>
2022-06-05 19:17:45 -0300

    Pantalla  de armado de equipo v0.1

---
+++ crud_dynamicTable/dynamicTable/codigo.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){     
+  $("#myTable").dynamicTable({
+        //definimos las columnas iniciales    
+        columns: [{
+              text: "Nombre",
+              key: "nombre"
+          },
+          {
+              text: "Apellido",
+              key: "apellido"
+          },
+          {
+              text: "Teléfono",
+              key: "telefono"
+          },
+          {
+              text: "DNI",
+              key: "dni"
+          },        
+        ],
+ 
+        //carga de datos
+        data: [{
+              nombre: 'Ana',
+              apellido: 'Perez',
+              telefono: '1532222222',
+              dni: '40500500'    
+              },
+        ],
+
+        //definición de botones
+        buttons: {
+            addButton: '<input type="button" value="Nuevo" class="btn btn-success" />',
+            cancelButton: '<input type="button" value="Cancelar" class="btn btn-primary" />',
+            deleteButton: '<input type="button" value="Borrar" class="btn btn-danger" />',
+            editButton: '<input type="button" value="Editar" class="btn btn-primary" />',
+            saveButton: '<input type="button" value="Guardar" class="btn btn-success" />',
+        },
+        showActionColumn: true,
+        //condicionales
+        getControl: function (columnKey) {
+            if (columnKey == "age") {
+              return '<input type="number" class="form-control" />';
+            }
+
+            if (columnKey == "gender") {
+              return '<select class="form-control"><option value="M">Masculino</option><option value="F">Femenino</option></select>';
+            }
+
+            return '<input type="text" class="form-control" />';
+        }
+
+    });	    
+});    
