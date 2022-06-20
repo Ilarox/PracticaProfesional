29755e658d1e71222928b622bb116e728913d97d
Ilana Rojas <rojas.cintia11@gmail.com>
2022-06-20 16:48:04 -0300

    se habilita  access controll origin for all en nodejs (el backend)

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const port = 3002;
 const app = express();
 
+// Configurar cabeceras y cors
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
 
 // Start the server
 const server = app.listen(port, (error) => {
@@ -10,18 +18,7 @@ const server = app.listen(port, (error) => {
     console.log(`Server listening on port ${server.address().port}`);
 });
 
-
-app.get('/crear-usuario', (request, response) => {
-    // Website you wish to allow to connect
-    request.header('Access-Control-Allow-Origin', 'http://localhost:8888');
-    // Request methods you wish to allow
-    request.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    //request.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    //request.header('Access-Control-Allow-Credentials', true);
-
+app.post('/crear-usuario', (request, response) => {
     console.log("llegue aca");
     var json = JSON.stringify({ 
         'success': true, 
@@ -29,9 +26,7 @@ app.get('/crear-usuario', (request, response) => {
         'error': false
     });
     response.send(json);
-
 });
-
 
 
 app.get('/login', (request, response) => {
